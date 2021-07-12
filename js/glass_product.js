7804fefc2fcb58eae4e223817db3be6f84659676
tommylam0402 <51775151+tommylam0402@users.noreply.github.com>
2021-07-12 12:15:08 +0700

    Update glass_product.js

--- js/glass_product.js
+++ js/glass_product.js
@@ -1243,13 +1243,12 @@ var shoppingCart = (function() {
         if(cart[item].name === name) {
           cart[item].count ++;
           saveCart();
-          return;
+          return loadCart();
         }
       }
       var item = new Item(name, price, count, img);
       cart.push(item)++;
       saveCart()++;
-      return loadCart()++;
     }
     // Set count from item
     obj.setCountForItem = function(name, count) {
